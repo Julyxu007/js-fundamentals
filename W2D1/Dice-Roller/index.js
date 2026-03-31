a9ddf35cc5937e957013d6e8450850e34d4922f1
julyxu <julyxu0720@gmail.com>
2026-03-31 21:42:43 +1300

    diceroll program

---
+++ W2D1/Dice-Roller/index.js
@@ -0,0 +1,14 @@
+function rollDice() {
+  const numOfDice = document.getElementById("dice-count").value;
+  const diceResult = document.getElementById("dice-result");
+  const diceImages = document.getElementById("dice-images");
+  const values = [];
+  const images = [];
+  for (let i = 0; i < numOfDice; i++) {
+    const value = Math.floor(Math.random() * 6 + 1);
+    values.push(value);
+    images.push(`<img src="dice-images/${value}.png">`);
+  }
+  diceResult.textContent = `Random Roll: ${values.join(", ")}`;
+  diceImages.innerHTML = images.join(``);
+}
